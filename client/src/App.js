b0532e1fca4a411930014a7e7533c323609e5569
Mats Hemå <89969770+mhema42@users.noreply.github.com>
2022-04-02 16:43:25 +0200

    add simple word chooser from array

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,20 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  let result
-  let letters
-  let correctWord = "wordle";
+  let result = "";
+  let letters  = [];
+  const [correctWord, setWord] = useState([]);
   const [text, setText] = useState("");
   const [guesses, setGuess] = useState([]);
   const [chkWord, wordle] = useState("");
+
+  const words = ["", "test", "mats", "wordle"];
+
+  const whichWord = (event) => {
+    setWord(words[event.target.value]);
+  };
+
+  console.log(correctWord)
 
   const onTextChange = (event) => {
     setText(event.target.value);
@@ -20,38 +28,38 @@ function App() {
       {
         word: text,
       },
-    ]);
-    
+    ]); 
+    // check if guess is correct
     if (text === correctWord) {
       wordle("Your guess is correct, congratulation!");
     }
     else {
       wordle("");
-
+      // split correctWord and guess to array
+      let correctLetters = correctWord.split("");
       letters = text.split("").map((letter, index) => ({
         ...index,
           letter: letter,
           status: "incorrect",
       }));
-
-      correctWord = correctWord.split("");
-    
-      if (letters.length !== correctWord.length) {
-        wordle("your guess must contain " + correctWord.length + " letters");
+      // check if guess contains correct amount of letters
+      if (letters.length !== correctLetters.length) {
+        wordle("your guess must contain " + correctLetters.length + " letters");
       }
+      // check if guess contains correct, incorrect or missplaced letters
       else {
           for (let i=0; i < letters.length; i++) {
-              if (letters[i].letter === correctWord[i]) {
+              if (letters[i].letter === correctLetters[i]) {
                   letters[i].status = "correct"
-                  correctWord[i] = "check"
+                  correctLetters[i] = "check"
               };
           };
           letters.forEach(element => {    
               for (let j=0; j < letters.length; j++) {
                   if (element.status === "incorrect") {
-                      if (element.letter === correctWord[j]) {
+                      if (element.letter === correctLetters[j]) {
                           element.status = "missplaced"
-                          correctWord[j] = "check"
+                          correctLetters[j] = "check"
                       };
                   };
               };
@@ -89,6 +97,7 @@ function App() {
         <i>- a fantastic world of words -</i>
         <p>Begin by choosing number of letters in Word to guess for,</p>
         <p>and if it should contain only unique letters or not</p>
+        <input type = "text" value= { text } onChange = { whichWord } />
         <input type = "text" value= { text } onChange = { onTextChange } />
         <button onClick = { onClickOk }>OK</button>
         <ul>{ guessElements }</ul>
